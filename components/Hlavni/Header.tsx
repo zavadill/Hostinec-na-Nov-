f8a9ef6634efe197d3cb010d5d14b9f6ac5c9b26
Adam Zavadil <adamzav33@seznam.cz>
2025-05-14 12:21:38 +0200

    Adds basic website layout and styling
    Adds a basic website layout with a navigation bar and header.
    
    Defines custom colors as CSS variables and introduces basic button styles using Tailwind CSS.
    
    The navigation bar is now a separate component with scrolling behavior.
    
    The main page content is replaced with a header component.

---
+++ components/Hlavni/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className="text-[var(--foreground)]">
+      <div className='min-h-screen flex flex-col max-w-10/12 mx-auto justify-center space-y-5'>
+        <h1>Hostinec na Nové</h1>
+        <p>Tradiční česká kuchyně v srdci Hané od roku 1762</p>
+        <div className='flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-5'>
+          <Link href="/" className='custom-btn-green'>Prohlédnout menu</Link>
+          <Link href="/" className='custom-btn-brown'>Rezervovat stůl</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ components/Vedlejsi/Navbar.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import React from 'react'
+import Link from 'next/link'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const Navbar = () => {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        setScrolled(window.scrollY > 1);
+      };
+    
+      window.addEventListener('scroll', handleScroll);
+      handleScroll(); // >>> Přidej tohle volání!
+    
+      return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+  return (
+    <div className={`${scrolled ? ' bg-white text-black h-17' : ' bg-transparent text-white h-20'} fixed w-full transition-all duration-300 `}>
+        <div className="max-w-10/12 mx-auto flex justify-between items-center h-full">
+            <Link href="/" className='text-[var(--brown-btn)] text-2xl font-bold'>Hostinec na Nové</Link>
+            <div className='flex flex-row space-x-7 items-center'>
+                <Link href="/">Domů</Link>
+                <Link href="/onas">O nás</Link>
+                <Link href="/menu">Menu</Link>
+                <Link href="/prostory">Prostory</Link>
+                <Link href="/akce">Svatby a akce</Link>
+                <Link href="/galerie">Galerie</Link>
+                <Link href="/kontakt">Kontakt</Link>
+                <Link href="/rezervace" className='custom-btn-green'>Rezervovat</Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
