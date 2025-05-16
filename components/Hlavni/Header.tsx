36ca8881f09cc207d6ce674a97ed1efe61262266
Adam Zavadil <adamzav33@seznam.cz>
2025-05-16 10:55:55 +0200

    Enhances header appearance with background image
    Improves the visual appeal of the header by adding a background image.
    
    Implements a dark overlay to improve text readability against the background.

--- components/Hlavni/Header.tsx
+++ components/Hlavni/Header.tsx
@@ -3,8 +3,16 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className="text-[var(--white)] bg-amber-100">
-      <div className='min-h-screen flex flex-col max-w-10/12 mx-auto justify-center space-y-5'>
+    // Přidáme lineární gradient pro ztmavení a zajistíme, že obrázek je pod ním
+    <div
+      className="text-[var(--white)] bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/uvodni.jpg')" }} // Nastavení obrázku přes style pro lepší kombinaci s overlayem
+    >
+      {/* Overlay pro ztmavení */}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      {/* Obsah zůstává nad overlayem díky relativnímu pozicování a z-indexu (pokud by byl potřeba) */}
+      <div className='min-h-screen flex flex-col max-w-10/12 mx-auto justify-center space-y-5 relative z-10'>
         <h1 className='text-6xl'>Hostinec na Nové</h1>
         <p className='text-2xl'>Tradiční česká kuchyně v srdci Hané od roku 1762</p>
         <div className='flex flex-col md:flex-row gap-4'>
